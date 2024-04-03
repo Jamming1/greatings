1984cd09d8f91acb00d15f2bd526df409ecfdd79
JAMMING <154436983+Jamming1@users.noreply.github.com>
2024-04-03 18:22:37 +0200

    Update GreatingsController.java

--- src/main/java/com/example/greatings/GreatingsController.java
+++ src/main/java/com/example/greatings/GreatingsController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.*;
 public class GreatingsController {
     @GetMapping(path = "/ciao")
     public String ciao(@RequestParam String nome,@RequestParam String regione) {
-        return "Ciao " + nome + " !" + "Com'è il tempo in " + regione + " ?";
+        return "Ciao " + nome + "!" + "Com'è il tempo in " + regione + "?";
     }
 }
